efba5a4e800b366df4b1f016b1b35a15f3ea31dd
yeongno <dldudsh83@gmail.com>
2022-08-25 15:41:21 +0900

    set viewport_id

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
-viewport = document.querySelector("meta[name=viewport]");
-viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
