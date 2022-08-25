b92253674d2c5a60aa6e79b6ff699acfcfa3f8b8
yeongno <dldudsh83@gmail.com>
2022-08-25 15:34:02 +0900

    set viewport_id

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,7 @@ import Login from "./views/Login";
 import Women from "./views/Women";
 
 function App() {
-  viewport = document.querySelector("meta[name=viewport]");
-viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+
   return (
     <BrowserRouter>
       <Routes>
